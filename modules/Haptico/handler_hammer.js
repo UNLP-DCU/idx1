db1f94bc8db511671490d5174bcf745883af122e
Diego Paez <paez.di@gmail.com>
2015-06-11 08:29:53 -0300

    configurando reconocedor pan

--- modules/Haptico/handler_hammer.js
+++ modules/Haptico/handler_hammer.js
@@ -1,7 +1,7 @@
-/*! Version Hammer.JS - v2.0.4 - 2014-09-28 
+/*! Version Hammer.JS - v2.0.4 - 2014-09-28
  * http://hammerjs.github.io/
  *
- * Este script se encarga de los eventos hapticos 
+ * Este script se encarga de los eventos hapticos
  * manejados con la libreria Hammer */
 
 var myElement = document.getElementById('main-content');
@@ -9,18 +9,21 @@ var myElement = document.getElementById('main-content');
 // crea una instancia hammer
 var mc = new Hammer(myElement);
 
+// Configurar reconocedores...
+mc.add( new Hammer.Pan({ threshold: 80 }) );
+
 // Definiendo los eventos..
 /*--deslizado hacia derecha--*/
-mc.on("panright", function(ev) {
+mc.on("panright panleft", function(ev) {
     //myElement.textContent = ev.type +" >>>>>>>>>>";
-    console.log("Derecha!");
+    console.log( ev.type + "!");
      //metodo_cambiar();
 });
 
 /*--deslizado hacia izquierda--*/
-mc.on("panleft", function(ev) {
+/*mc.on("panleft", function(ev) {
     //myElement.textContent = ev.type +" <<<<<<<<<<<";
     console.log("izquierda!");
     //metodo_cambiar();
-});
- 
+});*/
+
