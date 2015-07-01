b6846cb68bff605a60a8a79a65ab1ede3b206ef4
wilber Rejas Tellez <wilk_r15@hotmail.com>
2015-06-30 22:13:26 -0300

    Agregado rol status

--- modules/Accesibility-cross/accessibility.js
+++ modules/Accesibility-cross/accessibility.js
@@ -10,8 +10,10 @@ function agregarTabDirectorios() {
      $('#directorios').children('.box').attr('tabindex',1);
 };
 
-
-
+//Setea el mensaje del estado de los cambios realizados
+function setMsg(description) {  
+	$('#msg').html("<span>"+description+"</span>");
+};
 
 //Se define una funcion para el evento de presionar tecla enter
 (function ($) {
@@ -31,12 +33,14 @@ $("#alterarOrden").enterPressed(function() {
     startUp.alterarOrden();
     console.log( "alterar Orden");
     agregarTabDirectorios();
+    setMsg("Se alteró el orden de los archivos");
 });
 
 $("#irHaciaArriba").enterPressed(function() {
     startUp.irHaciaArriba();
     console.log( "-alterar Orden");
     agregarTabDirectorios();
+    setMsg("Se subió un nivel en los archivos");
     
 });
 
@@ -44,29 +48,33 @@ $("#irHaciaAbajo").enterPressed(function() {
     startUp.irHaciaAdentro();
     console.log( "-abajo");
     agregarTabDirectorios();
+    setMsg("Se bajó un nivel en los archivos");
 });
 
 $("#izquierda").enterPressed(function() {
     startUp.irAIzquierda();
     console.log( "-izuqierda");
     agregarTabDirectorios();
+    setMsg("Se posicionó a la izquierda");
 });
 
 $("#derecha").enterPressed(function() {
     startUp.irADerecha();
     console.log( "-derecha");
     agregarTabDirectorios();
+    setMsg("Se posicionó a la derecha");
 });
 
 $("#actualizar").enterPressed(function() {
      startUp.cargarElementosPrincipales();
      console.log( "-actualizar");
-     agregarTadDirectorios();
+     agregarTabDirectorios();
+     setMsg("Se actualizó los archivos");
 });
 
 /* Agregando tab-index a los archivos obtenidos*/
 $( document ).ready(function() {
-   agregarTadDirectorios();
+   agregarTabDirectorios();
 });
 
 
